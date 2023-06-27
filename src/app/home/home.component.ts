a93ffe3da692fdd7134d7833e3d2a461a346bf52
Eric G <eric.gangloff08@gmail.com>
2023-06-27 11:45:30 +0200

    generell api query works

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,33 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Team {
+  allStar: boolean,
+  city: string,
+  code: string,
+  id: number,
+  leagues: any,
+  logo: string,
+  name: string,
+  nbaFranchise: boolean,
+  nickname: string
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+export class HomeComponent implements OnInit {
+  teams: Team[] = [];
 
-export class HomeComponent {
+  ngOnInit(): void {
+    this.getTeamsResponse()
+  }
+
   async getTeamsResponse() {
     try {
-      const response = await fetch("https://api-nba-v1.p.rapidapi.com/teams?id=&name=&code=&league=&conference=&division=", {
+      const response = await fetch("https://api-nba-v1.p.rapidapi.com/teams", {
         method: 'GET',
         headers: {
           'X-RapidAPI-Key': '395acc3761msh8c7b29a36b5430bp19a90fjsn07e2336b7c27',
@@ -20,7 +37,7 @@ export class HomeComponent {
 
       if (response.ok) {
         const result = await response.json();
-        console.log(result);
+        this.teams = result.response
       }
     } catch (err) {
       console.error(err);
