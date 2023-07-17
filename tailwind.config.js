1486b0b0519163edde05757aa85d5beb0e3d7ba0
Eric G <eric.gangloff08@gmail.com>
2023-07-17 11:24:50 +0200

    simple Design Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
   ],
   theme: {
     fontSize: {
-      teamName: ['10px'],
-      link: ['6px'],
-      teamHeader: ['25px']
+      teamName: ['12px'],
+      link: ['10px'],
+      teamHeader: ['35px']
     },
     extend: {},
   },
